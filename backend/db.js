98dcf7f7cf5e9b535c1ba028a8e10580d1be75c5
sai-oduri <saioduri95@gmail.com>
2024-07-25 23:50:54 +0530

    Added userSchema

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config;
+
+mongoose.connect(process.env.MONGO_URL);
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
