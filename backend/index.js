2ec52b1583eab1fbfc303054ab039c2f87f2ef15
sai-oduri <saioduri95@gmail.com>
2024-07-26 00:38:32 +0530

    Added Routes Structure

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const mainRouter = require("./routes/index");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/v1", mainRouter);
+
+app.listen(3000);
+
+
+//  /api/v1/user/signup
+//  /api/v1/user/signin
+//  /api/v1/user/chnagePassword
+
+//  /api/v1/account/tranferMooney
+//  /api/v1/account/balance
