231d5aaac0285651294210e45e5f01adf19bdfd3
sai-oduri <saioduri95@gmail.com>
2024-07-26 18:47:07 +0530

    Added Signup Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,50 @@
 const express = require("express");
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
 const router = express.Router();
+
+require("dotenv").config;
+
+// signup route
+
+const signupSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+
+router.post("/signup", async (req, res) => {
+    const body = req.body;
+    const { success } = signupSchema.safeParse(req.body);
+    if (!success) {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: body.username
+    })
+
+    if (user._id) {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbUser = await User.create(body);
+
+    const token = jwt.sign({
+        userId: dbUser._id
+    }, process.env.JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+
+})
 
 module.exports = router;
