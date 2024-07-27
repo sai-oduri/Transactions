807c0e50254549776f17ea2e5e1de5dbdfb80c96
sai-oduri <saioduri95@gmail.com>
2024-07-27 23:26:03 +0530

    Added Update Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const zod = require("zod");
 const jwt = require("jsonwebtoken");
 const { User } = require("../db");
 const router = express.Router();
-
+const { authMiddleware } = require("../middleware");
 require("dotenv").config;
 
 // signup route
@@ -46,5 +46,31 @@ router.post("/signup", async (req, res) => {
     })
 
 })
+
+// update
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional()
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    await User.updateOne({
+        id: req.userId
+    }, req.body)
+
+    res.json({
+        message: "Updates Successfully"
+    })
+})
+
 
 module.exports = router;
